6f5dd6ad946e4b39af2f46c2e32f4597320f8f26
theo begyn <theobegyn@live.com>
2025-09-10 23:34:33 +0200

    Add translations and entity service to dependency injection for Views

--- MarquitoUtils.MAUI/Class/Startup/DefaultStartup.cs
+++ MarquitoUtils.MAUI/Class/Startup/DefaultStartup.cs
@@ -1,30 +1,60 @@
-﻿using MarquitoUtils.Main.Class.Entities.Translation;
+﻿using MarquitoUtils.Main.Class.Entities.File;
+using MarquitoUtils.Main.Class.Entities.Translation;
+using MarquitoUtils.Main.Class.Service.Files;
 using MarquitoUtils.Main.Class.Service.General;
+using MarquitoUtils.Main.Class.Service.Sql;
+using MarquitoUtils.Main.Class.Sql;
 using System.Reflection;
 
 namespace MarquitoUtils.MAUI.Class.Startup
 {
-    public abstract class DefaultStartup
+    public abstract class DefaultStartup<TApp, DBContext>
+        where TApp : Application
+        where DBContext : DefaultDbContext
     {
         private MauiAppBuilder AppBuilder { get; }
-        protected DefaultStartup(MauiAppBuilder builder)
+        protected DefaultStartup(MauiAppBuilder builder, Assembly assembly)
         {
             this.AppBuilder = builder;
 
-            this.ManageTranslations();
+            this.AppBuilder
+                .UseMauiApp<TApp>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
 
-            this.ManageViews(builder);
+            this.ManageTranslations(assembly);
+
+            this.ManageEntityService(assembly);
+
+            this.ManageViews(this.AppBuilder);
         }
 
-        private void ManageTranslations()
+        private void ManageTranslations(Assembly assembly)
         {
             ITranslateService translateService = new TranslateService();
 
-            List<Translation> translations = translateService.GetTranslations(Properties.Resources.translateFilePath, Assembly.GetExecutingAssembly());
+            List<Translation> translations = translateService
+                .GetTranslations(Properties.Resources.translateFilePath, assembly);
 
             translateService = new TranslateService(translations);
 
             this.AppBuilder.Services.AddSingleton(translateService);
+        }
+
+        private void ManageEntityService(Assembly assembly)
+        {
+            // Init startup db contexts
+            IFileService fileService = new FileService();
+            IEntityService entityService = new EntityService();
+
+            DatabaseConfiguration databaseConfiguration = fileService
+                .GetDefaultDatabaseConfiguration(assembly);
+            entityService.DbContext = DefaultDbContext.GetDbContext<DBContext>(databaseConfiguration);
+
+            this.AppBuilder.Services.AddSingleton(entityService);
         }
 
         protected abstract void ManageViews(MauiAppBuilder builder);
--- MarquitoUtils.MAUI/Class/Views/DefaultView.cs
+++ MarquitoUtils.MAUI/Class/Views/DefaultView.cs
@@ -1,6 +1,48 @@
-﻿namespace MarquitoUtils.MAUI.Class.Views
+﻿using MarquitoUtils.Main.Class.Service.General;
+using MarquitoUtils.Main.Class.Service.Sql;
+using MarquitoUtils.Main.Class.Tools;
+using System.Globalization;
+using static MarquitoUtils.Main.Class.Enums.EnumLang;
+
+namespace MarquitoUtils.MAUI.Class.Views
 {
-    public abstract class DefaultView
+    public abstract partial class DefaultView : ContentView, IContentView
     {
+        protected IServiceProvider ServiceProvider { get; }
+        protected ITranslateService TranslateService { get; }
+        protected IEntityService EntityService { get; }
+
+        protected DefaultView(IServiceProvider serviceProvider, ITranslateService translateService,
+            IEntityService entityService)
+        {
+            this.ServiceProvider = serviceProvider;
+            this.TranslateService = translateService;
+            this.EntityService = entityService;
+        }
+
+        /// <summary>
+        /// Get current language
+        /// </summary>
+        /// <returns>The current language</returns>
+        protected LanguageType GetCurrentLanguage()
+        {
+            return LanguageUtils.GetLanguage(CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Get translation for a class and a translation key
+        /// </summary>
+        /// <typeparam name="T">The class need the translation</typeparam>
+        /// <param name="translationKey">The translation key for translate</param>
+        /// <param name="language">The language for translate</param>
+        /// <returns>The translation</returns>
+        protected string GetTranslation<T>(string translationKey)
+        {
+            return this.TranslateService.GetTranslation<T>(translationKey, this.GetCurrentLanguage());
+        }
+
+        public abstract void Init();
+
+        protected abstract void ManageTranslations();
     }
 }
