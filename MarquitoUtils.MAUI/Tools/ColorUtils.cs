4fec546b66b21fd5c312f2c25c912ee87019f783
theo begyn <theobegyn@live.com>
2026-04-12 22:05:59 +0200

    Add class for color tool methods

---
+++ MarquitoUtils.MAUI/Tools/ColorUtils.cs
@@ -0,0 +1,29 @@
+﻿namespace MarquitoUtils.MAUI.Tools
+{
+    public static class ColorUtils
+    {
+        public static Color ConvertColorToMAUIColor(this System.Drawing.Color color)
+        {
+            return Color.FromRgba($"#{color.R:X2}{color.G:X2}{color.B:X2}{color.A:X2}");
+        }
+
+        public static Color GetOppositeColor(this Color color, ResourceDictionary resources,
+            string lightThemeColorName, string darkThemeColorName)
+        {
+            Color oppositeColor;
+
+            if (color.GetLuminosity() > 0.5f)
+            {
+                oppositeColor = resources.TryGetValue(lightThemeColorName, out object fontColor)
+                    ? (Color)fontColor : Colors.Black;
+            }
+            else
+            {
+                oppositeColor = resources.TryGetValue(darkThemeColorName, out object fontColor)
+                    ? (Color)fontColor : Colors.White;
+            }
+
+            return oppositeColor;
+        }
+    }
+}
