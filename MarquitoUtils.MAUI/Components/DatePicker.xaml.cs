18385cd388631caf414b2ff95bbb0509a31046df
theo begyn <theobegyn@live.com>
2026-04-26 19:24:09 +0200

    Add date picker

---
+++ MarquitoUtils.MAUI/Components/DatePicker.xaml.cs
@@ -0,0 +1,44 @@
+namespace MarquitoUtils.MAUI.Components;
+
+/// <summary>
+/// Date picker component
+/// </summary>
+public partial class DatePicker : Component
+{
+    #region Label
+
+    public static readonly BindableProperty LabelProperty = CreateProperty<string, TextBox>(nameof(Label), default(string));
+    /// <summary>
+    /// Label of the date picker
+    /// </summary>
+    public string Label
+    {
+        get => this.GetValue<string>(LabelProperty);
+        set => SetValue(LabelProperty, value);
+    }
+
+    #endregion Label
+
+    #region Date
+
+    public static readonly BindableProperty DateProperty = CreateProperty<DateTime, DatePicker>(nameof(Date));
+    /// <summary>
+    /// Value of the date picker
+    /// </summary>
+    public DateTime Date
+    {
+        get => this.GetValue<DateTime>(DateProperty);
+        set => SetValue(DateProperty, value);
+    }
+
+    #endregion Date
+
+    public DatePicker()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnComponentLoaded(object? sender, EventArgs e)
+    {
+    }
+}
