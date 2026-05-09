1039a1a994279a2be3d1fc22b96cc69064e55c2d
theo begyn <theobegyn@live.com>
2026-05-09 17:55:28 +0200

    Add Translate service to PageModel

--- MarquitoUtils.MAUI/Pages/Models/PageModel.cs
+++ MarquitoUtils.MAUI/Pages/Models/PageModel.cs
@@ -1,5 +1,10 @@
-﻿using MarquitoUtils.MAUI.Common.Models;
+﻿using MarquitoUtils.Main.Common.Tools;
+using MarquitoUtils.Main.Translate.Services;
+using MarquitoUtils.Main.Translate.Tools;
+using MarquitoUtils.MAUI.Common.Models;
 using System.ComponentModel;
+using System.Globalization;
+using static MarquitoUtils.Main.Translate.Enums.Language.EnumLang;
 
 namespace MarquitoUtils.MAUI.Pages.Models
 {
@@ -9,8 +14,37 @@ namespace MarquitoUtils.MAUI.Pages.Models
     public abstract class PageModel : CommonModel, INotifyPropertyChanged
     {
         /// <summary>
+        /// Translation service used to get translations for the page, allowing the page to support multiple languages 
+        /// and provide a localized experience for users. It is injected through the constructor and can be used in derived classes to get translations for specific keys.
+        /// </summary>
+        private ITranslateService TranslateService { get; }
+
+        /// <summary>
         /// Title of the page
         /// </summary>
         public string Title { get; set; }
+
+        public PageModel()
+        {
+        }
+
+        public PageModel(ITranslateService translateService) : base()
+        {
+            this.TranslateService = translateService;
+        }
+
+        protected string GetTranslation<T>(string translationKey)
+        {
+            if (Utils.IsNotNull(this.TranslateService))
+            {
+                LanguageType language = LanguageUtils.GetLanguage(CultureInfo.CurrentCulture);
+
+                return this.TranslateService.GetTranslation<T>(translationKey, language);
+            }
+            else
+            {
+                return translationKey;
+            }
+        }
     }
 }
